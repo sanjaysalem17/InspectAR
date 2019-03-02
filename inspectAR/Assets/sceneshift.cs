e3a44aa556deefde9e0fe873a0c876a4f3bfdf61
Sanjay <sanjaysalem17@gmail.com>
2019-03-02 11:43:00 -0600

    everything
    subete

---
+++ inspectAR/Assets/sceneshift.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using UnityEngine.UI;
+
+public class Sceneshift : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Begin()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
