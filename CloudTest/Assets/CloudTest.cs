e4d2b073b598d422bec67e982dd9e39448780da9
Yenlin Kuo <yliankuo@yahoo.com>
2019-03-01 22:42:29 -0500

    Database Stuff

---
+++ CloudTest/Assets/CloudTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class CloudTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://sh-cloud-test.firebaseio.com/");
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        FirebaseDatabase.DefaultInstance
+  .GetReference("Brian_Mac")
+  .GetValueAsync().ContinueWith(task => {
+      if (task.IsFaulted)
+      {
+              // Handle the error...
+          }
+      else if (task.IsCompleted)
+      {
+          DataSnapshot snapshot = task.Result;
+              // Do something with snapshot...
+              Debug.Log(snapshot);
+          }
+  });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
